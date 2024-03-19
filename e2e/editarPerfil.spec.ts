7bc64237061205fa359be57ed834df6157c589ed
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-03-19 19:06:09 -0300

    Criando teste de editar perfil

---
+++ e2e/editarPerfil.spec.ts
@@ -0,0 +1,13 @@
+import { test } from './page-objects/PaginaPerfil';
+
+test.describe("Página de Perfil", () => {
+  test.beforeEach(async ({ paginaLogin, paginaPerfil }) => {
+    await paginaLogin.visitar();
+    await paginaLogin.fazerLogin('antonio.evaldo@alura.com', '123456');
+    await paginaLogin.loginFeitoComSucesso();
+
+    await paginaPerfil.visitar();
+  });
+
+  test("Deve conseguir editar o perfil", async () => {  });
+});
