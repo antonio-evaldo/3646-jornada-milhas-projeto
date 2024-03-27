8e59cecaffe33f1c80db5f795b939027259bae51
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-03-27 12:08:25 -0300

    Adicionando cadastro no teste de login

--- e2e/operacoes/gerarPefil.ts
+++ e2e/operacoes/gerarPefil.ts
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker/locale/pt_BR';
 export function gerarPerfil(): Perfil {
   return {
     nome: faker.person.fullName(),
-    dataNascimento: faker.date.birthdate({ mode: 'age', min: 18, max: 120 }),
+    dataNascimento: faker.date.birthdate(),
     genero: faker.helpers.enumValue(Genero),
     cpf: faker.string.numeric(11),
     telefone: faker.string.numeric(11),
